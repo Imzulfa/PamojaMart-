44b2cc0cf507984ccfce672eab17144fedbd6475
Msafiri Mziray <buffchinksmziray@gmail.com>
2026-07-15 18:12:08 +0300

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -1,1 +1,37 @@
+exports.handler = async function(event) {
+  try {
+    const { paymentId, txid } = JSON.parse(event.body);
 
+    const response = await fetch(
+      `https://api.minepi.com/v2/payments/${paymentId}/complete`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Key ${process.env.PI_API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          txid: txid
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: response.status,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
