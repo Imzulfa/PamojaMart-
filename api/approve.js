c7a65b1dd925be6f0494cd2945a1a6f55d15110c
Msafiri Mziray <buffchinksmziray@gmail.com>
2026-07-15 17:55:04 +0300

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,1 +1,32 @@
+exports.handler = async function(event) {
+  try {
+    const { paymentId } = JSON.parse(event.body);
 
+    const response = await fetch(
+      `https://api.minepi.com/v2/payments/${paymentId}/approve`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Key ${process.env.PI_API_KEY}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: response.status,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
