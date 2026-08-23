4c207e29c7c8282e9f12ab81de44b94b55c54ea4
Msafiri Mziray <buffchinksmziray@gmail.com>
2026-08-23 16:21:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Endpoint ya kuthibitisha tokeni
 app.post('/api/auth/pi', async (req, res) => {
   const { accessToken } = req.body;
 
